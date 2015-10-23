93a847b7a7009a4ff739470721eebb0b42dadd50
Gordon Fontenot <gmoney@gitpushgitpaid.com>
2015-10-23 18:57:20 -0400

    Refactor tests
    `toDigits` and `toDigitsRev` have some shared behavior. We can extract
    that out into its own function for simplicity.

--- spring-2013/hw1/test/CreditCardValidatorSpec.hs
+++ spring-2013/hw1/test/CreditCardValidatorSpec.hs
@@ -11,11 +11,16 @@ spec = do
     describe "toDigits" $ do
         it "returns the digits as a list" $
             toDigits 12345 `shouldBe` [1, 2, 3, 4, 5]
-        it "returns an empty list for negative numbers" $
-            toDigits (-17) `shouldBe` []
+        digitParserSharedBehavior toDigits
+
     describe "toDigitsRev" $ do
         it "returns the digits as a list in reverse order" $
             toDigitsRev 12345 `shouldBe` [5, 4, 3, 2, 1]
+        digitParserSharedBehavior toDigitsRev
+
+digitParserSharedBehavior :: (Integer -> [Integer]) -> SpecWith ()
+digitParserSharedBehavior f =  do
         it "returns an empty list for negative numbers" $
-            toDigitsRev (-17) `shouldBe` []
-            
+            f (-17) `shouldBe` []
+        it "returns an empty list for 0" $
+            f 0 `shouldBe` []
